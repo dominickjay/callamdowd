6e569581b2ef5be4ec2ac927bcbc4c16c981315d
dominickjay217 <dominickjay217@gmail.com>
2021-02-23 20:39:02 +0000

    Remove test form

--- pages/index.vue
+++ pages/index.vue
@@ -328,17 +328,6 @@
         </div>
     </div>
 
-    <form name="contact" netlify>
-      <p>
-        <label>Name <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Email <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
     <code>{{ person.fields }}</code>
 
   </div>
