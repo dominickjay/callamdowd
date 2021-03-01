2a1aac5fb3c982798c1d8309eb048b03e715c856
dominickjay217 <dominickjay217@gmail.com>
2021-03-01 15:53:19 +0000

    Rebuilding form

--- components/contact.vue
+++ components/contact.vue
@@ -1,53 +1,16 @@
 <template>
     <div class="contact contact-form">
         <form
-            name="contact-form"
+            name="ask-question"
             method="post"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
             >
-            <input type="hidden" name="site-form" value="contact-form" />
-            <fieldset>
-                <legend>Risus, morbi metus, est bibendum elit nisl morbi nisl. Feugiat interdum.</legend>
-                <div>
-                    <label for="firstname">
-                        <span>First Name:</span>
-                        <input id="firstname" type="text" name="firstname">
-                        <abbr title="required" aria-label="required">*</abbr>
-                    </label>
-                </div>
-                <div>
-                    <label for="lastname">
-                        <span>Last Name:</span>
-                        <input id="lastname" type="text" name="lastname">
-                        <abbr title="required" aria-label="required">*</abbr>
-                    </label>
-                </div>
-                <div>
-                    <label for="email">
-                        <span>Email:</span>
-                        <input id="email" type="text" name="email">
-                        <abbr title="required" aria-label="required">*</abbr>
-                    </label>
-                </div>
-                <div>
-                    <label for="telnumber">
-                        <span>Phone Number:</span>
-                        <input id="telnumber" type="text" name="telnumber">
-                        <abbr title="required" aria-label="required">*</abbr>
-                    </label>
-                </div>
-                <div>
-                    <label for="message">
-                        <span>Message:</span>
-                        <textarea id="message" type="text" name="message"></textarea>
-                        <abbr title="required" aria-label="required">*</abbr>
-                    </label>
-                </div>
-                <div>
-                    <button class="button">Send Message</button>
-                </div>
-            </fieldset>
+            <input type="hidden" name="form-name" value="ask-question" />
+            <p>
+                <label>Your Name: <input type="text" name="name" /></label>
+            </p>
+            <button>Submit</button>
         </form>
     </div>
 </template>
