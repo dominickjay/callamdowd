366b6a52e1f63ca1b4d1dc16a0812263b917e7dd
dominickjay217 <dominickjay217@gmail.com>
2021-03-01 14:03:00 +0000

    Add hidden input for form

--- components/contact.vue
+++ components/contact.vue
@@ -1,5 +1,6 @@
 <template>
     <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="contact" value="contact" />
         <p>
             <label>Your Name: <input type="text" name="name" /></label>
         </p>
